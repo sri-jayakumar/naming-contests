2981eb818cb43b0656b9eb83cd91b3d5c55250a8
Sri <srijayakumar99@gmail.com>
2022-04-30 17:59:48 -0400

    setup initial structure

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      impliedStrict: true,
+      jsx: true,
+    },
+    sourceType: "module",
+  },
+  plugins: ["react", "react-hooks"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    indent: ['error', 2, {SwitchCase: 1}],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-console': ['warn', {allows: ['clear', 'info', 'error', 'dir', 'trace']}],
+    // You can do more rule customizations here...
+  },
+};
